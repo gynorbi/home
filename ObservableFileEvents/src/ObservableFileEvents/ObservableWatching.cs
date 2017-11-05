39a02ebee9d791c50221d5f4ffd40d60e0db0943
gynorbi <gynorbi@NORBI-LENOVO>
2017-11-05 16:37:50 +0200

    Initial commit with event based FSW and reactive FSW too.

---
+++ ObservableFileEvents/src/ObservableFileEvents/ObservableWatching.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace ObservableFileEvents
+{
+    public class ObservableWatching
+    {
+        FileSystemWatcher fsw;
+
+        public ObservableWatching()
+        {
+            fsw = new FileSystemWatcher(@"c:\temp\fsw");
+            fsw.IncludeSubdirectories = true;
+            fsw.EnableRaisingEvents = true;
+            var changeEvents = Observable
+                .FromEventPattern<FileSystemEventArgs>(fsw, "Changed");
+            var createdEvents = Observable
+                .FromEventPattern<FileSystemEventArgs>(fsw, "Created");
+            var renamedEvents = Observable
+                .FromEventPattern<FileSystemEventArgs>(fsw, "Renamed");
+            var events = Observable
+                .Merge(changeEvents, createdEvents, renamedEvents);
+            events.Subscribe(e =>
+               {
+                   Console.WriteLine($"'{e.EventArgs.Name}' has been {e.EventArgs.ChangeType}");
+               });
+            Console.WriteLine($"Watching of '{fsw.Path}' has started...");
+        }
+    }
+}
---
+++ ObservableFileEvents/src/ObservableFileEvents/StandarWatching.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObservableFileEvents
+{
+    public class StandarWatching
+    {
+        FileSystemWatcher fsw;
+
+        public StandarWatching()
+        {
+            fsw = new FileSystemWatcher(@"c:\temp\fsw");
+            fsw.Changed += Fsw_Changed;
+            fsw.Created += Fsw_Changed;
+            fsw.Renamed += Fsw_Changed;
+            fsw.IncludeSubdirectories = true;
+            fsw.EnableRaisingEvents = true;
+            Console.WriteLine($"Watching of '{fsw.Path}' has started...");
+
+        }
+
+        private void Fsw_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine($"'{e.Name}' has been {e.ChangeType}");
+        }
+    }
+}
