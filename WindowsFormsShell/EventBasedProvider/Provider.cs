7b9196e090da10b9096bfaf0c681ca8720aa96ca
gynorbi <gynorbi@NORBI-LENOVO>
2017-11-05 18:45:05 +0200

    Added basic classes.

---
+++ WindowsFormsShell/EventBasedProvider/Provider.cs
@@ -0,0 +1,80 @@
+﻿using CommonThings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBasedProvider
+{
+    public class Provider
+    {
+        FileSystemWatcher fsw;
+        string path = @"c:\temp\fsw";
+        IDictionary<string, List<FileData>> data;
+
+        public int Count { get { return data.Aggregate(0, (count, kvp) => count + kvp.Value.Count); } }
+
+        public Provider()
+        {
+            fsw = new FileSystemWatcher(path);
+            fsw.IncludeSubdirectories = true;
+            fsw.EnableRaisingEvents = true;
+            data = new Dictionary<string, List<FileData>>();
+        }
+
+        public async Task ReadAllFiles()
+        {
+            var allFileInfo = (new DirectoryInfo(path)).EnumerateFiles("*.txt", SearchOption.AllDirectories);
+            var tasks = new List<Task<string>>();
+            foreach (var p in allFileInfo)
+            {
+                var content = await ReadTextAsync(p.FullName);
+                ProcessResult(p.Name, p.FullName, content);
+            }
+
+            var results = await Task.WhenAll(tasks);
+        }
+
+        private void ProcessResult(string name, string fullName, string content)
+        {
+            if (data.ContainsKey(name))
+            {
+                var matchingFileData = data[name].FirstOrDefault(fd => fd.FullPath == fullName);
+                if (matchingFileData != null)
+                {
+                    matchingFileData.Content = content;
+                }
+                else
+                {
+                    data[name].Add(new FileData { FullPath = fullName, Content = content });
+                }
+            }
+            else
+            {
+                data.Add(name, new List<FileData>() { new FileData { FullPath = fullName, Content = content } });
+            }
+        }
+
+        private async Task<string> ReadTextAsync(string filePath)
+        {
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096, useAsync: true))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
--- WindowsFormsShell/WindowsFormsShell/Form1.cs
+++ WindowsFormsShell/WindowsFormsShell/Form1.cs
@@ -12,9 +12,29 @@ namespace WindowsFormsShell
 {
     public partial class Form1 : Form
     {
+        EventBasedProvider.Provider provider;
         public Form1()
         {
             InitializeComponent();
+            provider = new EventBasedProvider.Provider();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add($"Current count in data: {provider.Count}");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(
+                () => provider.ReadAllFiles().Wait()
+            );
         }
     }
 }
